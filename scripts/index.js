d9a28658763fa2a4f68f951c387a8fae08e15f98
Andrey Morogin <andreandreandre10071998@yandex.ru>
2022-06-29 18:49:42 +0300

    feat: добавить submit

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,25 @@
-let body = document.querySelector(".page");
-let popup = body.querySelector(".popup");
-let openButton = body.querySelector(".profile__edit-button");
+let popup = document.querySelector(".popup");
+let openButton = document.querySelector(".profile__edit-button");
 let closeButton = popup.querySelector(".popup__button-close");
-let saveButton = popup.querySelector(".popup__botton-save");
 let input = popup.querySelectorAll("input");
-let textName = body.querySelector(".profile__name");
-let textAboutMe = body.querySelector(".profile__about-me");
+let textName = document.querySelector(".profile__name");
+let textAboutMe = document.querySelector(".profile__about-me");
 
-function openPopup() {
-  body.classList.add("page_none-scroll");
+function openPopup () {
   popup.classList.add("popup_active");
 }
 
-function closePopup() {
-  body.classList.remove("page_none-scroll");
+function closePopup () {
   popup.classList.remove("popup_active");
 }
 
-function savePopup() {
-  textName.insertAdjacentText("afterbegin", input[0].value);
-  textAboutMe.insertAdjacentText("afterbegin", input[1].value);
+function formSubmitHandler (evt) {
+  textName.textContent = input[0].value;
+  textAboutMe.textContent = input[1].value;
   closePopup();
+  evt.preventDefault();
 }
 
 openButton.addEventListener("click", openPopup);
+popup.addEventListener("submit", formSubmitHandler);
 closeButton.addEventListener("click", closePopup);
-saveButton.addEventListener("click", savePopup);
