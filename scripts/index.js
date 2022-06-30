19d7f63fa8828a1fdcf899fa5c9a2bc9b47af583
Andrey Morogin <andreandreandre10071998@yandex.ru>
2022-06-30 16:19:37 +0300

    fix: js html

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,29 @@
-let popup = document.querySelector(".popup");
-let openButton = document.querySelector(".profile__edit-button");
-let closeButton = popup.querySelector(".popup__button-close");
-let input = popup.querySelectorAll("input");
-let textName = document.querySelector(".profile__name");
-let textAboutMe = document.querySelector(".profile__about-me");
+let popup = document.querySelector('.popup');
+let form = popup.querySelector('.popup__form');
+let openButton = document.querySelector('.profile__edit-button');
+let closeButton = popup.querySelector('.popup__button-close');
+let inputName = form.querySelector('.popup__input-name');
+let inputAboutMe = form.querySelector('.popup__input-about-me');
+let textName = document.querySelector('.profile__name');
+let textAboutMe = document.querySelector('.profile__about-me');
 
 function openPopup () {
-  input[0].value = textName.textContent;
-  input[1].value = textAboutMe.textContent;
-  popup.classList.add("popup_active");
+  inputName.value = textName.textContent;
+  inputAboutMe.value = textAboutMe.textContent;
+  popup.classList.add('popup_active');
 }
 
 function closePopup () {
-  popup.classList.remove("popup_active");
+  popup.classList.remove('popup_active');
 }
 
 function formSubmitHandler (evt) {
-  textName.textContent = input[0].value;
-  textAboutMe.textContent = input[1].value;
+  textName.textContent = inputName.value;
+  textAboutMe.textContent = inputAboutMe.value;
   closePopup();
   evt.preventDefault();
 }
 
-openButton.addEventListener("click", openPopup);
-popup.addEventListener("submit", formSubmitHandler);
-closeButton.addEventListener("click", closePopup);
+openButton.addEventListener('click', openPopup);
+form.addEventListener('submit', formSubmitHandler);
+closeButton.addEventListener('click', closePopup);
