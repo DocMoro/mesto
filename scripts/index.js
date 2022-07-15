6821311a5fdcbc34551bb7c705b157ff3e8cc19e
Andrey Morogin <andreandreandre10071998@yandex.ru>
2022-07-15 19:54:33 +0300

    feat: добавить карточки

--- scripts/index.js
+++ scripts/index.js
@@ -6,6 +6,44 @@ let inputName = popupForm.querySelector('.popup__input_field_name');
 let inputAboutMe = popupForm.querySelector('.popup__input_field_about-me');
 let textName = document.querySelector('.profile__name');
 let textAboutMe = document.querySelector('.profile__about-me');
+let cardsList = document.querySelector('.cards');
+
+function createCardList() {
+  const cards = [
+    {
+      name: 'Карачаевск',
+      link: './images/Карачаевск.jpg'
+    },
+    {
+      name: 'Гора Эльбрус',
+      link: './images/ГораЭльбрус.jpg'
+    },
+    {
+      name: 'Домбай',
+      link: './images/Домбай.jpg'
+    },
+    {
+      name: 'Гора Эльбрус',
+      link: './images/ГораЭльбрус.jpg'
+    },
+    {
+      name: 'Домбай',
+      link: './images/Домбай.jpg'
+    },
+    {
+      name: 'Карачаево-Черкесия',
+      link: './images/Карачаевск.jpg'
+    }
+  ];
+  cards.forEach(createCard);
+}
+
+function createCard(cardDate) {
+  const newCard = document.querySelector('.template-card').content.querySelector('.card').cloneNode(true);
+  newCard.querySelector('.card__title').textContent = cardDate.name;
+  newCard.querySelector('.card__image').src = cardDate.link;
+  cardsList.appendChild(newCard);
+}
 
 function openPopup () {
   inputName.value = textName.textContent;
@@ -24,6 +62,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
 }
 
+createCardList();
 openButton.addEventListener('click', openPopup);
 popupForm.addEventListener('submit', formSubmitHandler);
 closeButton.addEventListener('click', closePopup);
