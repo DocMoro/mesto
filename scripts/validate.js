24dbc02025e720b377f77da6b17d4bf6f9bfeb44
Andrey Morogin <andreandreandre10071998@yandex.ru>
2022-08-02 20:38:25 +0300

    fix: вынести данные валидации в глобальную константу

--- scripts/validate.js
+++ scripts/validate.js
@@ -7,64 +7,64 @@ const config = {
   errorClass: 'popup__error_visible'
 };
 
-function showInputError(config, formElement, inputElement, errorMessage) {
+function showInputError(formElement, inputElement, errorMessage) {
   const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
   inputElement.classList.add(config.inputErrorClass);
   errorElement.textContent = errorMessage;
 }
 
-function hideInputError(config, formElement, inputElement) {
+function hideInputError(formElement, inputElement) {
   const errorElement = formElement.querySelector(`.${inputElement.name}-error`);
   inputElement.classList.remove(config.inputErrorClass);
   errorElement.textContent = '';
 }
 
-function enableValidation(config) {
+function enableValidation() {
   const formList = Array.from(document.querySelectorAll(config.formSelector));
   formList.forEach(formElement => {
     formElement.addEventListener('submit', evt => {
       evt.preventDefault();
     });
-    setEventListeners(config, formElement, config.inputSelector);
+    setEventListeners(formElement, config.inputSelector);
   });
 }
 
-function setEventListeners(config, formElement) {
+function setEventListeners(formElement) {
   const inputList = Array.from(formElement.querySelectorAll(config.inputSelector));
   const buttonElement = formElement.querySelector(config.submitButtonSelector);
-  disableButton(config, buttonElement);
+  disableButton(buttonElement);
   formElement.addEventListener('reset', evt => {
     inputList.forEach((inputElement) => {
-      hideInputError(config, evt.currentTarget, inputElement)
+      hideInputError(evt.currentTarget, inputElement)
     });
-    disableButton(config, buttonElement);
+    disableButton(buttonElement);
   });
   inputList.forEach(inputElement => {
     inputElement.addEventListener('input', () => {
-      checkInputValidity(config, formElement, inputElement);
-      toggleButtonState(config, inputList, buttonElement);
+      checkInputValidity(formElement, inputElement);
+      toggleButtonState(inputList, buttonElement);
     });
   });
 }
 
-function checkInputValidity(config, formElement, inputElement) {
+function checkInputValidity(formElement, inputElement) {
   if (!inputElement.validity.valid) {
-    showInputError(config, formElement, inputElement, inputElement.validationMessage);
+    showInputError(formElement, inputElement, inputElement.validationMessage);
   } else {
-    hideInputError(config, formElement, inputElement);
+    hideInputError(formElement, inputElement);
   }
 }
 
-function toggleButtonState(config, inputList, buttonElement) {
+function toggleButtonState(inputList, buttonElement) {
   if (hasInvalidInput(inputList)) {
-    disableButton(config, buttonElement);
+    disableButton(buttonElement);
   } else {
     buttonElement.classList.remove(config.inactiveButtonClass);
     buttonElement.removeAttribute('disabled');
   }
 }
 
-function disableButton(config, buttonElement) {
+function disableButton(buttonElement) {
   buttonElement.classList.add(config.inactiveButtonClass);
   buttonElement.setAttribute('disabled', '');
 }
@@ -73,4 +73,4 @@ function hasInvalidInput(inputList) {
   return inputList.some(inputElement => !inputElement.validity.valid);
 }
 
-enableValidation(config);
+enableValidation();
